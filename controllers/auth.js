0680fe1ba231a3140e62934f112385ad72344c04
Mehmood_Dev <127541405+MehmoodCanvas@users.noreply.github.com>
2025-04-18 05:09:23 +0500

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,14 +7,14 @@ require('dotenv').config();
 exports.showLogin = (req,res)=> res.render('login');
 exports.showSignup = (req,res)=> res.render('signup');
 
-exports.login=(req,res)=>{
-    const {members_email,members_password}= req.body;
-    Member.findByEmail(members_email,(err,results)=>{
-        if( results.length > 0){
-            const member= results[0];
-                bcrypt.compare(members_password, member.members_password, (err, match) => {
-                if(match){
-
+exports.login = (req, res) => {
+    const { members_email, members_password } = req.body;
+    
+    Member.findByEmail(members_email, (err, results) => {
+        if (results.length > 0) {
+            const member = results[0];
+            bcrypt.compare(members_password, member.members_password, (err, match) => {
+                if (match) {
                     const token = jwt.sign(
                         {
                             id: member.members_id,
@@ -24,28 +24,40 @@ exports.login=(req,res)=>{
                         process.env.JWT_SECRET,
                         { expiresIn: process.env.JWT_EXPIRES_IN || '7d' }
                     );
-                    res.send({ status: 200 ,token:token, data:member });
-                }else{
+                    res.send({ status: 200, token: token, data: member });
+                } else {
                     res.send(err);
                 }
-            })
-        }else{
+            });
+        } else {
             res.send(err);
-
         }
-
-    })
-}
-exports.register = (req, res) => {
-    const { members_first_name,members_last_name, members_email , members_password } = req.body;
-    bcrypt.hash(members_password, 10, (err, hash) => {
-      const newUser = {members_first_name,members_last_name, members_email, members_password: hash };
-    Member.createMember(newUser, (err, result) => {
-      if (err) {
-        res.send({ error: 'Error creating member', details: err });
-      } else {
-        res.send({ message: 'Member created successfully', data: result });
-      }
     });
+};
+exports.register = (req, res) => {
+    const { members_first_name, members_last_name, members_email, members_password } = req.body;
+    
+    bcrypt.hash(members_password, 10, (err, hash) => {
+        const newUser = {
+            members_first_name,
+            members_last_name,
+            members_email,
+            members_password: hash
+        };
+        
+        Member.createMember(newUser, (err, result) => {
+            if (err) {
+                res.send({ error: 'Error creating member', data: err });
+            } else {
+                const token = jwt.sign(
+                    {
+                        id: result.members_id,
+                        email: result.members_email,
+                        name: `${result.members_first_name} ${result.members_last_name}`
+                    },
+                );
+                res.send({ status: 200, token: token, data: result });
+            }
+        });
     });
 };
