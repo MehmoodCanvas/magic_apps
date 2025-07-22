67fde252985ff7f7202e1a2d674fbfa1a5a24f39
huzaifa-canvas <m.huzaifa@canvasdigital.net>
2025-07-22 22:09:38 +0500

    some fixes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\FeedPostController;
+use App\Http\Controllers\API\FeedPostController;
 use App\Http\Controllers\API\UserProfileController;
 
 /*
